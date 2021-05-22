c727db1ff550bc623583f4eb3f2e07b7105bd50e
Yunus Emre Halis <yemrehalis@gmail.com>
2021-05-22 18:43:29 +0300

    API fetch, listview

--- coin-list/components/CoinList.js
+++ coin-list/components/CoinList.js
@@ -1,3 +1,22 @@
-export default function CoinList() {
-    return <></>;
+import Coins from "./Coins";
+
+export default function CoinList({filteredCoins}) {
+    return (
+        <>
+            {filteredCoins.map(coin => {
+                return (
+                    <Coins
+                    key={coin.id}
+                    name={coin.name}
+                    price={coin.current_price}
+                    symbol={coin.symbol}
+                    marketcap={coin.market_cap}
+                    volume={coin.total_volume}
+                    image={coin.image}
+                    priceChange={coin.price_change_percentage_24h}
+                    />
+                );
+            })}
+        </>
+    );
 }
